68b2a4248c41c5f6ad52b7f45b046abc6488e0c5
Karim Hossenbux <karimhossenbux@gmail.com>
2018-07-27 23:25:57 +0200

    first open source repo, first commit 🔥

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <VueConfetti :numberOfPieces="150" />
+  </div>
+</template>
+
+<script>
+import VueConfetti from './components/VueConfetti.vue'
+
+export default {
+  name: 'app',
+  components: {
+    VueConfetti
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
---
+++ src/components/VueConfetti.vue
@@ -0,0 +1,74 @@
+<template>
+    <canvas></canvas>
+</template>
+
+<script>
+import confetti from './confetti'
+
+export default {
+  name: 'VueConfetti',
+  mounted: function () {
+    this.$el.width = window.innerWidth
+    this.$el.height = window.innerHeight
+    confetti(this.$el).numberOfPieces(this.numberOfPieces)
+      .friction(this.friction)
+      .wind(this.wind)
+      .gravity(this.gravity)
+      .colors(this.colors)
+      .opacity(this.opacity)
+      .recycle(this.recycle)
+      .run(this.run)()
+  },
+  props: {
+    numberOfPieces: {
+      type: Number,
+      default: 200
+    },
+    friction: {
+      type: Number,
+      default: 0.99
+    },
+    wind: {
+      type: Number,
+      default: 0
+    },
+    gravity: {
+      type: Number,
+      default: 0.1
+    },
+    colors: {
+      type: Array,
+      default: function () {
+        return ['#f44336', '#e91e63', '#9c27b0', '#673ab7', '#3f51b5',
+        '#2196f3', '#03a9f4', '#00bcd4', '#009688', '#4CAF50',
+        '#8BC34A', '#CDDC39', '#FFEB3B', '#FFC107', '#FF9800',
+        '#FF5722', '#795548']
+      }
+    },
+    opacity: {
+      type: Number,
+      default: 1.0
+    },
+    recycle: {
+      type: Boolean,
+      default: true
+    },
+    run: {
+      type: Boolean,
+      default: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+  canvas {
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    pointer-events: none;
+    z-index: 1000;
+  }
+</style>
